e3f13e40f7ae6e3b9e262024b4e2c076acfc58b9
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-06 17:09:23 -0400

    Implemented wxIMPLEMENT_APP() command in CalculatorApp.cpp;

--- Calculator_JP/CalculatorApp.cpp
+++ Calculator_JP/CalculatorApp.cpp
@@ -1,5 +1,7 @@
 #include "CalculatorApp.h"
 
+wxIMPLEMENT_APP(CalculatorApp); // Command that sets up the entry point for the app; "int main{}"
+
 bool CalculatorApp::OnInit()
 {
 	mainFrame = new CalculatorFrame();
