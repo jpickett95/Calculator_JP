88af9ec1499d6820617a8c4ca2f251d4fa844fc6
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-06 17:04:31 -0400

    Declared OnInit() function in CalculatorApp & framed definition in .cpp;

--- Calculator_JP/CalculatorApp.cpp
+++ Calculator_JP/CalculatorApp.cpp
@@ -1,1 +1,6 @@
 #include "CalculatorApp.h"
+
+bool CalculatorApp::OnInit()
+{
+	return false;
+}
--- Calculator_JP/CalculatorApp.h
+++ Calculator_JP/CalculatorApp.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "wx/wx.h"
+#include "wx/wx.h" // include wxWidgets
 
-class CalculatorApp : public wxApp
+class CalculatorApp : public wxApp // inherits from wxApp
 {
+public:
+	virtual bool OnInit(); // used for wxWidgets to run
 };
 
