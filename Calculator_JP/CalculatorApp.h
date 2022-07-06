543307c5ad7bfd35a59839feab200a14d53b00a4
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-06 17:11:05 -0400

    Added comments;

--- Calculator_JP/CalculatorApp.h
+++ Calculator_JP/CalculatorApp.h
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+* ***********Solution Properties***************
+* C/C++ -> Additional Include Directories: C:\wxWidgets\include; C:\wxWidgets\include\msvc
+* Linker -> Additional Library Directories: C:\wxWidgets\lib\vc_lib
+* Linker -> System -> SubSystem: Windows(/SUBSYSTEM:WINDOWS)
+*/
+
 #include "wx/wx.h" // include wxWidgets
 #include "CalculatorFrame.h"
 
