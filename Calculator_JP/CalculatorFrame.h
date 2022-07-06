12b7f3c2b9ef57a59c8fbecdbbf48c8cf367031c
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-06 17:26:55 -0400

    Declared button pointers;

--- Calculator_JP/CalculatorFrame.h
+++ Calculator_JP/CalculatorFrame.h
@@ -4,6 +4,30 @@
 
 class CalculatorFrame : public wxFrame // inherits from wxFrame
 {
+private:
+	// Buttons
+	wxButton* numButton_0;
+	wxButton* numButton_1;
+	wxButton* numButton_2;
+	wxButton* numButton_3;
+	wxButton* numButton_4;
+	wxButton* numButton_5;
+	wxButton* numButton_6;
+	wxButton* numButton_7;
+	wxButton* numButton_8;
+	wxButton* numButton_9;
+	wxButton* addButton;
+	wxButton* subtractButton;
+	wxButton* multiplyButton;
+	wxButton* divideButton;
+	wxButton* equalsButton;
+	wxButton* clearButton;
+	wxButton* modButton; // modulus
+	wxButton* decimalPointButton; // "."
+	wxButton* posNegButton; // +/-
+	wxButton* binButton; // Binary 
+	wxButton* hexButton; // Hexdecimal
+	wxButton* decButton; // Decimal
 public:
 	CalculatorFrame(); // Constructor
 };
