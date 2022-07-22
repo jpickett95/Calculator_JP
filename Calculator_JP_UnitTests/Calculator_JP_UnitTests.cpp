c6af7522f8a048125db7665efa885f24ac883bb9
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-22 15:32:58 -0400

    Outlined functions for unit tests;

--- Calculator_JP_UnitTests/Calculator_JP_UnitTests.cpp
+++ Calculator_JP_UnitTests/Calculator_JP_UnitTests.cpp
@@ -11,8 +11,68 @@ namespace CalculatorJPUnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
-		{
+		// ButtonFactory Tests
+		TEST_METHOD(FactoryTest1) {
+
+		}
+		TEST_METHOD(FactoryTest2) {
+
+		}
+		TEST_METHOD(FactoryTest3) {
+
+		}
+		TEST_METHOD(FactoryTest4) {
+
+		}
+		TEST_METHOD(FactoryTest5) {
+
+		}
+		TEST_METHOD(FactoryTest6) {
+
+		}
+		TEST_METHOD(FactoryTest7) {
+
+		}
+		TEST_METHOD(FactoryTest8) {
+
+		}
+		TEST_METHOD(FactoryTest9) {
+
+		}
+		TEST_METHOD(FactoryTest10) {
+
+		}
+
+		// CalculatorProcessor Tests
+		TEST_METHOD(ProcessorTest1) {
+
+		}
+		TEST_METHOD(ProcessorTest2) {
+
+		}
+		TEST_METHOD(ProcessorTest3) {
+
+		}
+		TEST_METHOD(ProcessorTest4) {
+
+		}
+		TEST_METHOD(ProcessorTest5) {
+
+		}
+		TEST_METHOD(ProcessorTest6) {
+
+		}
+		TEST_METHOD(ProcessorTest7) {
+
+		}
+		TEST_METHOD(ProcessorTest8) {
+
+		}
+		TEST_METHOD(ProcessorTest9) {
+
+		}
+		TEST_METHOD(ProcessorTest10) {
+
 		}
 	};
 }
