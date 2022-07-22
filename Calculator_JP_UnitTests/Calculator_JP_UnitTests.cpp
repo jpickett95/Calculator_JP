bd9f745e5672b001d5d2a2da7c27d814dffa3120
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-22 15:27:47 -0400

    created UnitTests project, and inlcuded ButtonFactory & CalculatorProcessor .h & .cpp files;

---
+++ Calculator_JP_UnitTests/Calculator_JP_UnitTests.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Calculator_JP/ButtonFactory.h"
+#include "../Calculator_JP/CalculatorProcessor.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorJPUnitTests
+{
+	TEST_CLASS(CalculatorJPUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
